df3e0cb42ccdaa79b73f19df37ab9979bc147407
mozumderTushar <56648029+mozumderTushar@users.noreply.github.com>
2020-08-06 23:35:02 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,10 @@
+let pakhi;
+function add (num1,num2){
+    console.log(num1 + num2);
+    return
+}
+const result = add(10,20)
+const lover = {name:'smart sankar', phone:1233435}
+
+
+console.log(lover.gf);
